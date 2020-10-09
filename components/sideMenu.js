fe9d725bf5ca0e3fd7e4fe5f390d5f6f04f2bb25
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-09 13:51:32 -0400

    adding in modal implementation and close modal for movie application

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import Modal from "./modal";
 
 const SideMenu = (props) => {
   const { categories } = props;
   return (
     <div>
+      <Modal />
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         {categories.map((c) => (

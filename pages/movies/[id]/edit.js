6f00e862550001293a6ab6ae5eba66ec1f06a037
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-10 16:18:01 -0400

    final touches on Update movies functionality

--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -1,43 +1,32 @@
 import react from "react";
-import Movie from ".";
-import { getMovieById } from "../../../actions";
+import Router from "next/router";
+import { getMovieById, updateMovie } from "../../../actions";
 import MovieCreateForm from "../../../components/movieCreateForm";
 
 class EditMovie extends React.Component {
-  //   static getInitialProps({ query }) {
-  //     return { query };
-  //   }
-
   static async getInitialProps({ query }) {
     const movie = await getMovieById(query.id);
 
     return { movie };
   }
 
-  //   state = {
-  //     movie: {
-  //       name: "",
-  //       description: "",
-  //       rating: "",
-  //       image: "",
-  //       cover: "",
-  //       longDesc: "",
-  //     },
-  //   };
-
-  //   componentDidMount() {
-  //     const { id } = this.props.query;
-  //     getMovieById(id).then((movie) => {
-  //       this.setState({ movie });
-  //     });
-  //   }
+  handleUpdateMovie = (movie) => {
+    updateMovie(movie).then((updatedMovie) => {
+      //   Router.push("/");
+      Router.push(`/movies/${movie.id}`);
+    });
+  };
 
   render() {
     const { movie } = this.props;
     return (
       <div className="container">
         <h1>Edit the Movie</h1>
-        <MovieCreateForm initialData={movie} />
+        <MovieCreateForm
+          submitButton="Update"
+          initialData={movie}
+          handleFormSubmit={this.handleCreateMovie}
+        />
       </div>
     );
   }

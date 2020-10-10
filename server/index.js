6ce0349354745b430bf3c28ad634b3b04244c734
Justin Ramirez <ramirezbjustin@gmail.com>
2020-10-10 13:06:12 -0400

    added in additional server routes for movie application

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const next = require("next");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -7,6 +8,32 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  server.use(bodyParser.json());
+
+  server.get("/api/v1/movies", (req, res) => {
+    return res.json({ message: "Hello World" });
+  });
+
+  server.post("/api/v1/movies", (req, res) => {
+    const movie = req.body;
+    console.log(JSON.stringify(movie));
+    return res.json({ ...movie, createdTime: "today", author: "Justin" });
+  });
+
+  server.patch("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Updating post of id: ${id}` });
+  });
+
+  server.delete("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Deleting post of id: ${id}` });
+  });
+
+  //   server.get("/faq", (req, res) => {
+  //     res.send("<html><head></head><body><h1> Hello World! </h1></body></html>");
+  //   });
+
   // we are handling all of the request coming to our server
   server.get("*", (req, res) => {
     // next.js is handling request and providing pages where we are navigating to
